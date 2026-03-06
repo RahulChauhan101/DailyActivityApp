a1833ee7e6564c0a72904e2c8479ae58fb97bc5d
RahulChauhan101 <rahul.chauhan12241@gmail.com>
2026-03-06 08:55:55 +0530

    new commit

--- App.jsx
+++ App.jsx
@@ -40,7 +40,7 @@ return(
 
 <View style={styles.container}>
 
-<Text style={styles.header}>Daily Activity  APP</Text>
+<Text style={styles.header}>Daily Activity  treking APP</Text>
 <Text style={styles.header}>Daily Planner</Text>
 
 <View style={styles.inputContainer}>
